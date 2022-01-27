be2ec2e5baf12616b81bf158c1d05c9ee128bc08
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-01-27 07:05:37 -0500

    fixed error, dev good prod good; last commit for config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import About from './components/About'
-import './App.css'
+//import './App.css'
 //import Navbar from './components/Navbar'
 //import Contact from './components/Contact'
 //import Projects from './components/Projects';
