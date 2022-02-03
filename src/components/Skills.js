1e1a823000933b6a9d2465f45352b47147fb2734
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-02-03 18:06:30 -0500

    proj. compo fin ., init skills compo

---
+++ src/components/Skills.js
@@ -0,0 +1,24 @@
+import { ChipIcon, BadgeCheckIcon } from '@heroicons/react/solid'
+import React from 'react'
+import { skills } from '../data'
+
+const Skills = () => {
+return (
+    <section>
+        <div>
+        <div>
+            <ChipIcon />
+            <h1></h1>
+            <p></p>
+        </div>
+        <div>
+            {skills.map((skill) => (
+                <div></div>
+            ))}
+        </div>
+        </div>
+    </section>
+)
+}
+
+export default Skills
