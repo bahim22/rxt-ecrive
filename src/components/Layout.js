2e010ea1d608a2be19f3f2aff4094b947452f2e0
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-02-04 14:13:54 -0500

    gen. layout compo add'l var

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,17 @@ import { any } from "prop-types"
 import { useState } from "react"
 
 const Layout = ({ header, children, footer, title, ...props  }) => {
-
+	const profile = {
+		name: 'Hima Balde',
+		title: 'web developer',
+		email: 'ibalde@isitymarket.onmicrosoft.com',
+		img: {
+			src:'./public/img/hima.png',
+			height:200,
+			width:200,
+			alt:''
+		}
+	}
     const [isShowing, setIsShowing] = useState(false)
     const [contact, setContact] = useState('')
 	return (
@@ -14,7 +24,7 @@ const Layout = ({ header, children, footer, title, ...props  }) => {
 				<div className="lg: flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
 						{' '}
-						Welcome to {header.title = 'HimaLand'}
+						Welcome to {profile.title = 'HimaLand'}
 						<br className="hidden lg:inline-block" /> Dev Portfolio
 					</h1>
 					<p className="mb-8 leading-relaxed">
