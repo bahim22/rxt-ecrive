f0a935e19d55bd4d43e58865dd5bf0c3d2a0a4c1
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-02-12 15:53:16 -0500

    ran eslint,prettier tasks & dev server; need to fix css, + pics; deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import About from './components/About'
-//import './App.css'
-//import Navbar from './components/Navbar'
-//import Contact from './components/Contact'
-//import Projects from './components/Projects';
-//import Skills from './components/Skills';
-//import Testimonials from './components/Testimonials';
+import './App.css'
+import Navbar from './components/Navbar'
+import Contact from './components/Contact'
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Software from './components/Software';
 
 function App() {
-	return (
-		<main className="text-gray-400 bg-gray-900 body-font">
-			{/*<Navbar />*/}
-			<About />
-			{/*<Projects />
+  return (
+    <main className='text-gray-400 bg-gray-900 body-font'>
+      <Navbar />
+      <About />
+      <Projects />
       <Skills />
-      <Testimonials />
-      <Contact />*/}
-		</main>
-	)
+      <Software />
+      <Contact />
+    </main>
+  )
 }
 
 export default App
--- src/data.js
+++ src/data.js
@@ -1,118 +1,115 @@
-
 export const projects = [
-	{
-		title: 'Next.js Blog',
-		subtitle: 'DioBlog',
-		description: 'React based JS website with custom styling and tool config.',
-		image: './img/bbman.png',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'Next.js Boilerplate',
-		subtitle: 'Next-ded-boiler',
-		description: 'React based JS website with custom styling and tool config.',
-		image: './public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'Next.js TailwindCss App',
-		subtitle: 'next-tail-22',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'React.js Boilerplate',
-		subtitle: 'react-ded-boiler',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'React.js Azure Authorization',
-		subtitle: 'auth-react-azure',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'Mern Stack Todo App',
-		subtitle: 'react-todo',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'Mern Stack template',
-		subtitle: 'ded-az',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'Mern FullStack Dashboard',
-		subtitle: 'menan-ded',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/react-portfo2',
-	},
-	{
-		title: 'ISM Company Site',
-		subtitle: 'ism',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/ism',
-	},
-	{
-		title: 'Html, Css, JS Portfolio',
-		subtitle: 'dioportfo',
-		description: 'React based JS website with custom styling and tool config.',
-		image: 'public/purp9.pngd',
-		link: 'https://github.com/bahim22/dioportfo',
-	}
+  {
+    title: 'Next.js Blog',
+    subtitle: 'DioBlog',
+    description: 'React based JS website with custom styling and tool config.',
+    image: './img/bbman.png',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'Next.js Boilerplate',
+    subtitle: 'Next-ded-boiler',
+    description: 'React based JS website with custom styling and tool config.',
+    image: './public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'Next.js TailwindCss App',
+    subtitle: 'next-tail-22',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'React.js Boilerplate',
+    subtitle: 'react-ded-boiler',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'React.js Azure Authorization',
+    subtitle: 'auth-react-azure',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'Mern Stack Todo App',
+    subtitle: 'react-todo',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'Mern Stack template',
+    subtitle: 'ded-az',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'Mern FullStack Dashboard',
+    subtitle: 'menan-ded',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/react-portfo2',
+  },
+  {
+    title: 'ISM Company Site',
+    subtitle: 'ism',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/ism',
+  },
+  {
+    title: 'Html, Css, JS Portfolio',
+    subtitle: 'dioportfo',
+    description: 'React based JS website with custom styling and tool config.',
+    image: 'public/purp9.pngd',
+    link: 'https://github.com/bahim22/dioportfo',
+  },
 ]
 
 export const futureProjects = [
-	{
-		title: 'Marketing and Comms for NPOs',
-		Info: 'Fullstack Mern Web App',
-	},
-	{
-		title: 'Mern Workout Scheduler',
-		Info: 'AzureCosmosDB',
-	},
-	{
-		title: 'Next.js Business Analytics',
-		Info: 'Progressive Web App',
-	},
+  {
+    title: 'Marketing and Comms for NPOs',
+    Info: 'Fullstack Mern Web App',
+  },
+  {
+    title: 'Mern Workout Scheduler',
+    Info: 'AzureCosmosDB',
+  },
+  {
+    title: 'Next.js Business Analytics',
+    Info: 'Progressive Web App',
+  },
 ]
 
 export const softwares = [
-	{
-		name: 'Azure',
-		tools: 'Dev, Saas',
-		image: 'public/purp9.pngd',
-	},
-	{
-		name: 'GitHub',
-		tools: 'Dev, hosting, collab, Version Control',
-		image: 'public/purp9.pngd',
-	},
-	{
-		name: 'Jira',
-		tools: 'Dev, Planning, workflow, Kanban Board',
-		image: 'public/purp9.pngd',
-	},
-	{
-		name: 'Office 365',
-		tools: 'office, excel, powerpoint, Planning, Admin',
-		image: 'public/purp9.pngd',
-	},
+  {
+    name: 'Azure',
+    tools: 'Dev, Saas',
+    image: 'public/purp9.pngd',
+  },
+  {
+    name: 'GitHub',
+    tools: 'Dev, hosting, collab, Version Control',
+    image: 'public/purp9.pngd',
+  },
+  {
+    name: 'Jira',
+    tools: 'Dev, Planning, workflow, Kanban Board',
+    image: 'public/purp9.pngd',
+  },
+  {
+    name: 'Office 365',
+    tools: 'office, excel, powerpoint, Planning, Admin',
+    image: 'public/purp9.pngd',
+  },
 ]
 
-export const skills = [
-	'JavaScript', 'TypeScript', 'JSX', 'JSON', 'HTML', 'CSS'
-]
+export const skills = ['JavaScript', 'TypeScript', 'JSX', 'JSON', 'HTML', 'CSS']
 
 //1. Edit: skills into object containing arrays for langauges, tools, frameworks, libs
 //2. Edit: software: adjust keys and add better value pairs
