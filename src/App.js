bbe5b8ff56630737f9ce0c6f4cd726c891c6c90a
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-03-14 09:14:12 -0400

    + project images; ran tasks; dev run: good; built for pro; todo: pics, edit desc., test, deploy

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react'
 import About from './components/About'
 import './App.css'
 import Contact from './components/Contact'
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Software from './components/Software';
-import Navbar from './components/Navbar';
-
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Software from './components/Software'
+import Navbar from './components/Navbar'
 
 function App() {
-  return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <Projects />
-      <About />
-      <Skills />
-      <Software />
-      <Contact />
-    </main>
-  )
+	return (
+		<main className='text-gray-400 bg-gray-900 body-font'>
+			<Navbar />
+			<About />
+			<Projects />
+			<Skills />
+			<Software />
+			<Contact />
+		</main>
+	)
 }
 
 export default App
