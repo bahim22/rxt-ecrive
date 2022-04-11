fc27d48c3b2b03ff53a2342c9a39d50decc9cd59
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-04-11 17:30:18 -0400

    + compos from practice repos; creating new UI stage 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import CardMain from './components/CardMain'
-
+import Form from './components/Form'
 function App() {
 	return (
 		<main className='text-gray-400 bg-gray-900 body-font'>
 			<div className='container px-5 py-24 mx-auto'>
 				<CardMain />
+				<Form />
 			</div>
 
 		</main>
